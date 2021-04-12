e8ad2f7c5779cccb8e6076fb2ed278909e51504d
Oxana Danilova <oxanadanilova11@gmail.com>
2021-04-12 22:02:41 +0200

    feat: solve 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,28 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let current = arr[0];
+  let counter = 0;
+  const result = [];
+  for (let i = 0; i < str.length; i += 1) {
+    if (arr[i] === current) {
+      counter += 1;
+      if (arr[i] !== arr[i + 1] || !arr[i + 1]) {
+        result.push(counter + current);
+      }
+    } else {
+      current = arr[i];
+      counter = 1;
+      if (arr[i] !== arr[i + 1] || !arr[i + 1]) {
+        result.push(counter + current);
+      }
+    }
+  }
+  const myString = result.join('');
+
+  return myString.replace(/1/g, '');
 }
 
 module.exports = encodeLine;
